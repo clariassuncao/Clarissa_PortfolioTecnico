92192465b334ca5261546b2ddc5d335854e99bef
Clarissa G. de Assunção <83765838+clariassuncao@users.noreply.github.com>
2022-04-20 11:49:43 -0300

    Create pavilhao.java

---
+++ ModelagemSistemas/Presidio/pavilhao.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class Pavilhao{
+  private int cela;
+  private int refeitorio;
+  private int banheiro;
+  private int solitaria;
+  private ArrayList<Presidiaro> Presidiario;
+  private ArrayList<Funcionario> Funcionario;
+  private int id;
+  private String tipo;
+  private int patio;
+  private int biblioteca;
+  private int lavanderia;
+  private int salaAula;
+  private ArrayList<String> salaTecnico;
+  private int capela;
+  private ArrayList<String> 
+  private boolean lojaConveniencia;
+
+  public Pavilhao (int cela, int refeitorio, int banheiro, int solitaria, int id, String tipo, int patio, int biblioteca, int lavanderia, int salaAula, int capela, boolean lojaConveniencia){
+    
+  }
+}
