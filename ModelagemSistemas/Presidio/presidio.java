8b5cdefb128b450eda0ab784a1f562017381026c
Clarissa G. de Assunção <83765838+clariassuncao@users.noreply.github.com>
2022-04-20 11:48:13 -0300

    Create presidio.java

---
+++ ModelagemSistemas/Presidio/presidio.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Presidio {
+  private String nome;
+  private String local;
+  private ArrayList<Pavilhao> pavilhao; 
+  private String contato;
+  private ArrayList<Pessoa> pessoa;
+  private ArrayList<String> estoque;
+  private float despesa;
+  private float orcamento;
+  private ArrayList<String> transporte;
+  private int lotacao;
+  private ArrayList<String> sistemaSeguranca;
+}
+
+public Presidio(String nome, String local, String contato, float despesa, float orcamento, int lotacao){
+  
+}
+               
