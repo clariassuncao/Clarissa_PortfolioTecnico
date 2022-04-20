6f003dddf1c0a4d1768a72bb1b47285bacbc8b6d
Clarissa G. de Assunção <83765838+clariassuncao@users.noreply.github.com>
2022-04-20 11:42:27 -0300

    Create pessoa.java

---
+++ ModelagemSistemas/Presidio/pessoa.java
@@ -0,0 +1,22 @@
+public class Pessoa{
+  private String nome;
+  private String cpf;
+  private String data_nasc;
+  private String contato;
+  private String genero;
+  private String endereco;
+
+
+public Pessoa(String nome, String cpf, String data_nasc, String contato, String genero, String endereco){
+  
+}
+  
+public void registrar(String nome, String cpf, String data_nasc, String contato, String genero, String endereco){
+  
+}
+
+public boolean verificarPresenca(String nome, String cpf){
+  return true;
+}
+
+  }
