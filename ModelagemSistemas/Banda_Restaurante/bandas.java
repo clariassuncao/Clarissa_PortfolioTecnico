1274035431b506aa732dab374af8980b3fe0950a
Clarissa G. de Assunção <83765838+clariassuncao@users.noreply.github.com>
2022-04-20 11:56:40 -0300

    Create bandas.java

---
+++ ModelagemSistemas/Banda_Restaurante/bandas.java
@@ -0,0 +1,32 @@
+public class Bandas{
+  private Artista artista;
+  private String instrumentos;
+  private int qtd_instrumentos;
+  private float valor_show;
+  private String genero_musical;
+  private String rede_social;
+  private String equipamentos;
+  private int qtd_equipamentos;
+  private int datas_disponiveis;
+  private String contato;
+
+  public Bandas(Artista artista, String instrumentos, int qtd_instrumentos, 
+ float valor_show, String genero_musical, String rede_social, String equipamentos, int 
+ qtd_equipamentos, int datas_disponiveis, String contato){  
+  }
+   public boolean levarInstrumentos(String instrumentos, int 
+   qtd_instrumentos){
+    return true;
+  }
+  public boolean levarAcustica(String equipamentos, int qtd_equipamentos){
+    return true;
+  }
+   public boolean afinarInstrumentos(String intrumentos){
+    return true;
+      }
+}
+
+
+  
+  
+  
