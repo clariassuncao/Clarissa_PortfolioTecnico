9c279645bda0f13f7cf59fe9821e264a54ef1e72
Clarissa G. de Assunção <83765838+clariassuncao@users.noreply.github.com>
2022-04-20 11:55:22 -0300

    Create pessoas.java

---
+++ ModelagemSistemas/Banda_Restaurante/pessoas.java
@@ -0,0 +1,16 @@
+public class Pessoa{
+  private String nome;
+  private String data_nasc;
+  private String endereco;
+  private String cpf;
+  private char genero;
+
+public Pessoa(String nome, String data_nasc, String endereco, String cpf, char genero){
+  
+}
+
+public boolean verificarPresenca(String nome, String cpf){
+  return true;
+}
+  
+  }
